06130fa5d3bbeaa7a2364ce89e07a95dfc38cc50
Кристина <1132246773@pfur.ru>
2026-04-01 15:50:30 +0300

    vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,30 +1,34 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
+import random
+import string
+import caesar
 
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+class vigenere:
+    @staticmethod
+    def encrypt_vigenere(plaintext: str, keyword):
+       lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        n = len(keyword)
+        ciphertext = list(plaintext)
+        for i in range(n):
+          if keyword[i] in lower:
+            shift = lower.find(keyword[i])
+          else:
+            shift = upper.find(keyword[i])
+          ciphertext[i::n] = caesar.encrypt_ceasar(plaintext[i::n], shift)
+          ciphertext = "".join(ciphertext)
+          return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        n = len(keyword)
+        ciphertext = list(ciphertext)
+        for i in range(n):
+            if keyword[i] in lower:
+            shift = lower.find(keyword[i])
+        else:
+            shift = upper.find(keyword[i])
+        cipher_text[i::n] = caesar.encrypt_ceasar(ciphertext[i::n], -shift)
+        cipher_text = "".join(cipher_text)
+        return cipher_text
