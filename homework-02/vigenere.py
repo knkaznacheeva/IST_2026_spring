984dbf7a47d426e1f2df221d5499680e7665ec3d
Кристина <1132246773@pfur.ru>
2026-05-28 01:05:17 +0300

    vigenere.py

--- homework-02/vigenere.py
+++ homework-02/vigenere.py
@@ -1,34 +1,51 @@
-import random
-import string
 import caesar
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
 
-class vigenere:
-    @staticmethod
-    def encrypt_vigenere(plaintext: str, keyword):
-       lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        n = len(keyword)
-        ciphertext = list(plaintext)
-        for i in range(n):
-          if keyword[i] in lower:
-            shift = lower.find(keyword[i])
-          else:
-            shift = upper.find(keyword[i])
-          ciphertext[i::n] = caesar.encrypt_ceasar(plaintext[i::n], shift)
-          ciphertext = "".join(ciphertext)
-          return ciphertext
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    ciphertext = ""
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    n = len(keyword)
+    cipher_text = list(plaintext)
+    for i in range(n):
+      if keyword[i] in lower:
+        shift = lower.find(keyword[i])
+      else:
+        shift = upper.find(keyword[i])
+      cipher_text[i::n] = caesar.encrypt_caesar(plaintext[i::n], shift)
+    cipher_text = "".join(cipher_text)
+    return cipher_text
 
 
-    def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        n = len(keyword)
-        ciphertext = list(ciphertext)
-        for i in range(n):
-            if keyword[i] in lower:
-            shift = lower.find(keyword[i])
-        else:
-            shift = upper.find(keyword[i])
-        cipher_text[i::n] = caesar.encrypt_ceasar(ciphertext[i::n], -shift)
-        cipher_text = "".join(cipher_text)
-        return cipher_text
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    plaintext = ""
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    n = len(keyword)
+    plaintext = list(ciphertext)
+    for i in range(n):
+      if keyword[i] in lower:
+        shift = lower.find(keyword[i])
+      else:
+        shift = upper.find(keyword[i])
+      plaintext[i::n] = caesar.encrypt_caesar(ciphertext[i::n], -shift)
+    plaintext = "".join(plaintext)
+    return plaintext
